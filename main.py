1f0166cf153c56ae43b88c35265dc97e4b3c40b1
Jack Roth <jackroth49@gmail.com>
2023-02-24 20:57:35 -0500

    starting structure

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('hello');
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/<name>')
+def index(name):
+    return render_template('header.html', username=name)
+
+@app.route('/login')
+def login():
+    return 'LOGIN PAGE'
+
+@app.route('/security')
+def security():
+    return 'Security Page'
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run()
+
